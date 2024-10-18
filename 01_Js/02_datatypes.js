fd3fa572358668734b8a083c244bde08546caa46
_sakshi <113840900+sakshi2003-code@users.noreply.github.com>
2024-10-18 07:10:47 +0000

    datatypes-done

---
+++ 01_Js/02_datatypes.js
@@ -0,0 +1,23 @@
+let name="sakshi"
+let id=1234
+let bool=true
+console.table([name,id,bool])
+
+// premetive
+// num=>2^53 range
+// string =>""
+// boolean => true or false
+// bigint => when we want to store no which has range greater than num like stoke market data
+// undefined
+// null => standalone value
+// symbol => used for uniqueness
+
+// non premetieve
+
+
+
+// object
+console.log(typeof undefined);
+// ans undefied
+console.log(typeof null)    
+// ans objece
