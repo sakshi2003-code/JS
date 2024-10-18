228e8df79485268ebc80c4e2a42e8f2906db6136
_sakshi <113840900+sakshi2003-code@users.noreply.github.com>
2024-10-18 10:10:15 +0000

    conversion_js

---
+++ 01_Js/03_conversion_operation.js
@@ -0,0 +1,44 @@
+// let number =123
+// let stringNum=String(number)
+// console.log(typeof stringNum); =>string
+
+// let str="1233"
+
+// let numString=Number(str)
+// console.log(typeof numString);
+
+
+// let str1="1233@sakshi"
+
+// let numString1=Number(str1)
+// console.log(numString1);Nan
+
+// console.log(typeof numString1); number
+
+
+
+// let str3=true
+
+// let numString3=Number(str3) 
+// console.log(numString3);  =>1
+
+// console.log(typeof numString3);=>number
+
+// let str5 =undefined
+// let numStr5=Number(str5)
+// console.log(numStr5); =>Nan
+
+// console.log(typeof numStr5);=>number
+
+
+// let str6 =null
+// let numStr6=Number(str6)
+// console.log(numStr6); =>0
+
+// console.log(typeof numStr6);=>number
+
+
+// Boolean
+// 1=>true , 0=>false
+// " " =>false
+// "sakshi"=>true
